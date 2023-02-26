e83ab6a4162c59185270a3ecaf6fc9b392446329
vadimagametov@gmail.com <Av19881988SsDemoni>
2023-02-26 23:40:57 +0400

    Оптимизация кода по принципу DRY.

--- tester.py
+++ tester.py
@@ -10,7 +10,7 @@ def main():
     url = input("Введите url страницы для проверки её времени загрузки: ")
 
     while True:
-        def get_page_load_time(url):
+        def get_page_params(url):
             if ("https" or "http") in url:  
                 open_this_url = urlopen(url)  
             else:
@@ -21,33 +21,14 @@ def main():
             end_time = time.time()  
             open_this_url.close() 
             time_to_load = end_time - start_time
-            return time_to_load
+            page_size = open_this_url.length
+            page_info = open_this_url.info()
+            return time_to_load, page_size, page_info
         time.sleep(1)   #Время между повторным запросом.
 
-        def get_page_info(url):
-            if ("https" or "http") in url:  
-                open_this_url = urlopen(url)  
-            else:
-                open_this_url = urlopen("https://" + url)  
 
-            page_info = open_this_url.info()            
-            
-            return page_info
-        time.sleep(1)   #Время между повторным запросом.
-
-        def get_page_size(url):
-            if ("https" or "http") in url:  
-                open_this_url = urlopen(url)  
-            else:
-                open_this_url = urlopen("https://" + url)  
-
-            page_size = open_this_url.length           
-            
-            return page_size
-        time.sleep(1)   #Время между повторным запросом.
-
-        print(f"\nВремя загрузки страницы {url} составляет {get_page_load_time(url):.2} секунды. \
-             \nРазмер страницы: {get_page_size(url)} байт. \nИнформация о странице: {get_page_info(url)}")
+        print(f"\nВремя загрузки страницы {url} составляет {get_page_params(url)[0]:.2} секунды. \
+             \nРазмер страницы: {get_page_params(url)[1]} байт. \nИнформация о странице: {get_page_params(url)[2]}")
         
         ###Завершение скрипта###
 
